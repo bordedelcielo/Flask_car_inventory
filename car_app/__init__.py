9b82e344f5f6a88f1414bd645f3d6be0c4c32317
bordedelcielo <christopher.thrutchley@gmail.com>
2021-11-09 23:18:18 -0600

    update to my homework for week 5.

--- car_app/__init__.py
+++ car_app/__init__.py
@@ -1,11 +1,23 @@
-from flask import Flask
-from config import Config
-from .site.routes import site
+from flask import Flask # importing the class Flask. __init__.py file will be the first item the app runs.
+from config import Config # import the Config from the file we just created.
+from .site.routes import site # linking Blueprint to the larger app.
 from .authentication.routes import auth
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate #https://flask-migrate.readthedocs.io/en/latest/
 
-app.register_blueprint(site)
+from .models import db as root_db # make the name more recognizeable.
+
+app = Flask(__name__) # Makes sure it attaches to the name of the folder.
+
+app.register_blueprint(site) # register a blueprint.
 app.register_blueprint(auth)
 
-app.config.from_object(Config)
+app.config.from_object(Config) # The flask app is now linked to Config.
+
+root_db.init_app(app)
+
+migrate = Migrate(app,root_db) #https://flask-migrate.readthedocs.io/en/latest/
+# Need to inherit app and root_db
+
+# from drone_inventory import models
--- car_app/authentication/routes.py
+++ car_app/authentication/routes.py
@@ -1,11 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from car_app.forms import UserLoginForm # import the UserLogin Form that
+# we just created in forms.html.
+from car_app.models import db, User # import a user
 
-auth = Blueprint('auth',__name__, template_folder = 'auth_templates')
+auth = Blueprint('auth',__name__, template_folder = 'auth_templates') # our blueprint
 
-@auth.route('/signup', methods = ['GET', 'POST'])
+@auth.route('/signup',methods = ['GET', 'POST']) # signup, register, etc.
 def signup():
-    return render_template('signup.html')
+    # return render_template('signup.html') # this was our original code
+    form = UserLoginForm()
 
-@auth.route('/signin', methods = ['GET', 'POST'])
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email, password)
+
+            user = User(email,password=password)
+            db.session.add(user)
+            db.session.commit()
+
+            flash(f'You have successfully created a user account for {email}.', "user-created") 
+
+            return redirect(url_for('site.home'))
+    except:
+        raise Exception('Invalid Form Data: Please check your form...')
+
+    return render_template('signup.html', form = form)
+
+@auth.route('/signin',methods = ['GET', 'POST']) # signup, register, etc.
 def signin():
     return render_template('signin.html')
